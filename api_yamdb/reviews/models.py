f1aa1eb5a95fb080c891787e12e9b57be45b457b
Аус Евгений <eugeneaus@yandex.ru>
2023-05-25 10:42:18 +0300

    added Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -84,3 +84,63 @@ class TitleGenre(models.Model):
 
     def __str__(self):
         return f'{self.title.name[:MAX_STR_TEXT_LIMIT]} {self.genre.name[:MAX_STR_TEXT_LIMIT]}'
+    
+
+class Review(models.Model):
+    """Модель с отзывами."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+        db_index=True
+    )
+    text = models.TextField()
+    score = models.IntegerField(
+        'Оценка',
+        default=0,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+
+    class Meta:
+        ordering = ['-pub_date'],
+        constrains = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review'
+            )
+        ]
+
+
+class Comment(models.Model):
+    """"Модель с комментариями."""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата комментария', 
+        auto_now_add=True,
+        db_index=True
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author
